7df270eace6ca5cba318e4ed2f9b86334f5afe02
Murlidhar Varma <murlidhar.varma@gmail.com>
2025-02-23 16:49:34 +0530

    Initial Draft

---
+++ src/main/java/org/aipeel/ragai/RagaiApplication.java
@@ -0,0 +1,13 @@
+package org.aipeel.ragai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aipeel/ragai/RagaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aipeel.ragai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
